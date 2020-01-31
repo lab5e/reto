aa9c77681c70038a50e7a8ad50b66c6da5e765b2
Ståle Dahl <stalehd@telenordigital.com>
2020-01-31 18:56:02 +0100

    Print status for changelog + config

--- pkg/commands/status_command.go
+++ pkg/commands/status_command.go
@@ -3,6 +3,7 @@ package commands
 import (
 	"fmt"
 
+	"github.com/ExploratoryEngineering/releasetool/pkg/changelog"
 	"github.com/ExploratoryEngineering/releasetool/pkg/release"
 )
 
@@ -10,29 +11,36 @@ type statusCommand struct {
 }
 
 func (c *statusCommand) Run(rc RunContext) error {
-	config, err := release.Verify()
+	ctx, err := release.Verify()
 	if err != nil {
 		return err
 	}
 
 	released := "NO"
-	if config.Released {
+	if ctx.Released {
 		released = "YES"
 	}
 
-	fmt.Printf("Active version: %s\n", config.Version)
+	fmt.Printf("Active version: %s\n", ctx.Version)
 	fmt.Printf("Released:       %s\n", released)
 	fmt.Println()
 	fmt.Println("Configuration:")
 	fmt.Println("  Targets: ")
-	for _, v := range config.Config.Targets {
+	for _, v := range ctx.Config.Targets {
 		fmt.Printf("  - %s\n", v)
 	}
 	fmt.Println("  Files:")
-	for _, v := range config.Config.Files {
+	for _, v := range ctx.Config.Files {
 		fmt.Printf("  - %s/%s\n", v.Name, v.Target)
 	}
 	fmt.Println()
 
+	if changelog.WorkingCopyIsComplete() == nil {
+		fmt.Println("Changelog is OK")
+	}
+
+	if release.VerifyConfig(ctx.Config) == nil {
+		fmt.Println("Configuration is OK")
+	}
 	return nil
 }
